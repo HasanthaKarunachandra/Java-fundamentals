df45280d3b325e46436cb9cf59749de4775af95a
Hasantha Karunachandra <32540627+HasanthaKarunachandra@users.noreply.github.com>
2023-05-02 21:14:32 +0530

    Singleton DesignPatton

---
+++ Design Patton/Singleton/Demo.java
@@ -0,0 +1,7 @@
+public class Demo {
+    public static void main(String[] args) {
+        Singleton singleton1= Singleton.getInstance();
+        Singleton singleton2= Singleton.getInstance();
+        System.out.println(singleton1==singleton2);
+    }
+}
